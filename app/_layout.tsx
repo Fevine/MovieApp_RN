cf725eb960a8c840be487870d60d54f06fc79868
Albinoni <albinoni1423@gmail.com>
2025-07-18 12:47:31 +0400

    main layout modified

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack screenOptions={{}}>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="movie/[id]"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen />
+    </Stack>
+  );
 }
