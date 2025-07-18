e2e587e646394627dbec313fef3dede5d393ed56
Albinoni <albinoni1423@gmail.com>
2025-07-18 16:17:41 +0400

    SearchBar created

---
+++ components/SearchBar.tsx
@@ -0,0 +1,50 @@
+import { Image, StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+import { Colors } from "@/constants/colors";
+import { icons } from "@/constants/icons";
+
+interface Props {
+  placeholder: string;
+  onPress: () => void;
+}
+
+export default function SearchBar({ placeholder, onPress }: Props) {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={icons.search}
+        style={styles.icon}
+        resizeMode="contain"
+        tintColor={"#ab8bff"}
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor={"#a8b5db"}
+        style={styles.searchInput}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.dark[200],
+    borderRadius: 9999,
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+  },
+  icon: {
+    width: 20,
+  },
+  searchInput: {
+    flex: 1,
+    marginLeft: 8,
+    color: "#ffffff",
+  },
+});
