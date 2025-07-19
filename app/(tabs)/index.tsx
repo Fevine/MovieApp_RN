bb8ed9143090ef147ca3bd458dd480bbaf301684
Albinoni <albinoni1423@gmail.com>
2025-07-19 07:19:07 +0400

    MovieData fetched

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,30 @@
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
 import { Colors } from "@/constants/colors";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/MovieCard";
 
 export default function index() {
   const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
 
   return (
     <View style={styles.container}>
@@ -18,12 +35,42 @@ export default function index() {
         contentContainerStyle={{ flexGrow: 1, paddingBottom: 10 }}
       >
         <Image source={icons.logo} style={styles.logo} />
-        <View style={{ marginTop: 20 }}>
-          <SearchBar 
-          onPress={() => router.push("/search")}
-          placeholder="Search for a movie"
+
+        {moviesLoading ? (
+          <ActivityIndicator
+            size={"large"}
+            color={"#0000ff"}
+            style={styles.activityIndicator}
           />
-        </View>
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ) : (
+          <View style={{ marginTop: 20 }}>
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie"
+            />
+
+            <>
+              <Text style={styles.latestTitle}>Latest Movies</Text>
+
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                style={styles.listWrapper}
+                scrollEnabled={false}
+              />
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
@@ -53,5 +100,21 @@ const styles = StyleSheet.create({
   scrollContentContainer: {
     minHeight: "100%",
     paddingBottom: 10,
+  },
+  activityIndicator: {
+    marginTop: 40,
+    alignSelf: "center",
+  },
+  latestTitle: {
+    fontSize: 18,
+    lineHeight: 22,
+    color: "#ffffff",
+    fontWeight: 700,
+    marginTop: 20,
+    marginBottom: 12,
+  },
+  listWrapper: {
+    marginTop: 8,
+    paddingBottom: 128,
   },
 });
