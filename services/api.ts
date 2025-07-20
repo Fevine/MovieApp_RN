9b1a9f313f3f51b3b20f2618856c559c3d1a916f
Albinoni <albinoni1423@gmail.com>
2025-07-20 10:29:38 +0400

    API: fetchMovieDetails added

--- services/api.ts
+++ services/api.ts
@@ -1,3 +1,4 @@
+
 export const TMDB_CONFIG = {
   BASE_URL: 'https://api.themoviedb.org/3',
   API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
@@ -27,6 +28,25 @@ export const fetchMovies = async ({ query }: { query: string }) => {
   return data.results
 }
 
+export const fetchMovieDetails = async (movieId: string): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`, {
+      method: 'GET',
+      headers: TMDB_CONFIG.headers,
+    })
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch movie details')
+    }
+
+    const data = response.json()
+
+    return data
+  } catch (error) {
+    console.log(error);
+    throw error
+  }
+}
 // const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
 // const options = {
 //   method: 'GET',
