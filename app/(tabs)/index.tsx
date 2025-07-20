d67eb7f9d370b83d2944597ca244cb9b0e4c4b91
Albinoni <albinoni1423@gmail.com>
2025-07-20 09:00:12 +0400

    HomeScreen: Trending movies section added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,9 +16,17 @@ import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
+import TrendingCard from "@/components/TrendingCard";
 
 export default function index() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
 
   const {
     data: movies,
@@ -36,14 +44,14 @@ export default function index() {
       >
         <Image source={icons.logo} style={styles.logo} />
 
-        {moviesLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator
             size={"large"}
             color={"#0000ff"}
             style={styles.activityIndicator}
           />
-        ) : moviesError ? (
-          <Text>Error: {moviesError?.message}</Text>
+        ) : moviesError || trendingError ? (
+          <Text>Error: {moviesError?.message || trendingError?.message}</Text>
         ) : (
           <View style={{ marginTop: 20 }}>
             <SearchBar
@@ -52,6 +60,24 @@ export default function index() {
               onChangeText={() => {}}
               value=""
             />
+
+            {trendingMovies && (
+              <View style={styles.trendingContainer}>
+                <Text style={styles.trendingTitle}>Trending Movies</Text>
+
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  ItemSeparatorComponent={() => <View style={{ width: 16 }} />}
+                  data={trendingMovies}
+                  renderItem={({ item, index }) => (
+                    <TrendingCard movie={item} index={index} />
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                  style={styles.trendingWrapper}
+                />
+              </View>
+            )}
 
             <>
               <Text style={styles.latestTitle}>Latest Movies</Text>
@@ -106,6 +132,20 @@ const styles = StyleSheet.create({
   activityIndicator: {
     marginTop: 40,
     alignSelf: "center",
+  },
+  trendingContainer: {
+    marginTop: 40,
+  },
+  trendingTitle: {
+    fontSize: 18,
+    lineHeight: 22,
+    color: "#ffffff",
+    fontWeight: 700,
+    marginBottom: 12,
+  },
+  trendingWrapper: {
+    marginTop: 20,
+    marginBottom: 12,
   },
   latestTitle: {
     fontSize: 18,
