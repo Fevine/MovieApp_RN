27708180f7e33832ce32f6c4fe36bcd3ea2a72e8
Albinoni <albinoni1423@gmail.com>
2025-07-20 10:58:44 +0400

    Saved screen modified

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,12 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Colors } from "@/constants/colors";
+import { icons } from "@/constants/icons";
 
-const Saved = () => {
+export default function Saved() {
   return (
-    <View>
-      <Text>Saved</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={icons.save}
+          tintColor={"#ffffff"}
+          style={styles.personIcon}
+        />
+        <Text style={styles.profileText}>Saved</Text>
+      </View>
     </View>
-  )
+  );
 }
 
-export default Saved
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.primary,
+    flex: 1,
+    paddingHorizontal: 40,
+  },
+  header: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    flexDirection: "column",
+    gap: 20,
+  },
+  personIcon: {
+    width: 40,
+    height: 40,
+  },
+  profileText: {
+    color: "#6B7280",
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
